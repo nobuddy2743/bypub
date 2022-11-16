fd314b064db779355c33001860ed724f236cfcdf
myexample001 <110930054+myexample001@users.noreply.github.com>
2022-11-16 16:52:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ from display_progress import progress_for_pyrogram, humanbytes
 
 
 # bot
-bot_token = os.environ.get("BOT_TOKEN")
-api_hash = os.environ.get("API_HASH") 
-api_id = os.environ.get("API_ID")
-log_ch = os.environ.get("LOG_CHANNEL")
+bot_token = Config.BOT_TOKEN
+api_hash = Config.API_HASH
+api_id = Config.API_ID
+log_ch = Config.LOG_CHANNEL
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
 
 
@@ -68,6 +68,22 @@ async def send_start(client: pyrogram.client.Client, message: pyrogram.types.mes
     reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton(" Share", url="https://t.me/link_bypass_kd_bot")]]), reply_to_message_id=message.id)
 
 
+# status command    
+@app.on_message(filters.private & filters.command("status") & filters.user(Config.BOT_OWNER))
+async def status(_,m: pyrogram.types.messages_and_media.message.Message):
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+    total_users = await db.total_users_count()
+    await m.reply_text(
+        text=f"**Total Disk Space:** {total} \n**Used Space:** {used}({disk_usage}%) \n**Free Space:** {free} \n**CPU Usage:** {cpu_usage}% \n**RAM Usage:** {ram_usage}%\n\n**Total Users in DB:** `{total_users}`",
+        #parse_mode="Markdown",
+        quote=True
+    )
 
 
 # help command
