3645c3bb8d94303b9f65be0d4a60af098ac72718
myexample001 <110930054+myexample001@users.noreply.github.com>
2022-11-16 21:57:50 +0530

    Update database.py

--- database.py
+++ database.py
@@ -29,8 +29,8 @@ class Database:
         #link_count += 1
         self.col.update_one({"$set":{"total_link_count":link_count}})
 
-    def find_one(self, uid):
-        return self.col.find_one({'id': uid})
+    async def find_one(self, id):
+        return await self.col.find_one({'id': int(id)})
 
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id': int(id)})
