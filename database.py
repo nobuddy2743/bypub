2abffa7b91f64d1d286fb94c577d65ba4b45d2d4
myexample001 <110930054+myexample001@users.noreply.github.com>
2022-11-16 20:10:44 +0530

    Update database.py

--- database.py
+++ database.py
@@ -31,15 +31,15 @@ class Database:
         
     def get_total_link(self):
         link_count =0
-        x = col.find({"new_user.total_link_count"})
+        x = self.col.find({"new_user.total_link_count"})
         for data in x:
             print(data.get('new_user')[0].get('total_link_count'))
             #link_count += 
         #link_count =0
         #for x in self.col.find({}, {"total_link_count": 1}):
-            link_count += 1
+            #link_count += 1
         #link_count += 1
-        return link_count    
+        #return link_count    
 
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id': int(id)})
