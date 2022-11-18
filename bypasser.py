655e11e510979eebc1c29fab44ad71837fca052d
myexample001 <110930054+myexample001@users.noreply.github.com>
2022-11-18 11:37:53 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -48,34 +48,9 @@ ddllist = ["disk.yandex.com","mediafire.com","uptobox.com","osdn.net","github.co
 ############################################################
 # Adrino links
 
-def adrinolinks(url):
-    client = cloudscraper.create_scraper(allow_brotli=False)
-    if ('adrinolinks.com' in url) or ('adrinolinks.in' in url):
-        DOMAIN = "https://wikitraveltips.com/"
-    else:
-        return "https://wikitraveltips.com/"
-
-    url = url[:-1] if url[-1] == '/' else url
-
-    code = url.split("/")[-1]
-    
-    final_url = f"{DOMAIN}/{code}"
-
-    resp = client.get(final_url)
-    soup = BeautifulSoup(resp.content, "html.parser")
-    
-    try: inputs = soup.find(id="safelink_redirect").find_all(name="input")
-    except: return "Incorrect Link"
-    
-    data = { input.get('name'): input.get('value') for input in inputs }
-
-    h = { "x-requested-with": "XMLHttpRequest" }
-    
-    time.sleep(5)
-    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try:
-        return r.json()['url']
-    except: return "Something went wrong :("
+def adrinolinks(url):    
+    bypassed_link = PyBypass.bypass(url, name="adrinolinks")
+    return bypassed_link
 
 
 ###############################################################
