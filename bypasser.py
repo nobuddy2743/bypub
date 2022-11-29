f3db1ced967f6cecfa887a6632f63922cdf023d5
myexample001 <110930054+myexample001@users.noreply.github.com>
2022-11-29 16:30:42 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -61,7 +61,7 @@ def adrino_links(url):
     
     final_url = f"{DOMAIN}/{code}"
     
-    ref = "https://wikitraveltips.com//"
+    ref = "https://wikitraveltips.com/"
     
     h = {"referer": ref}
   
@@ -832,28 +832,31 @@ def shareus(url):
 
 def shortlingly(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
-    if 'shortingly.me' in url:
-        DOMAIN = "https://loan.techyjeeshan.xyz"
-    else:
-        return "Incorrect Link"
+    
+    
+    DOMAIN = "https://shortingly.in/"
 
     url = url[:-1] if url[-1] == '/' else url
-
+    
     code = url.split("/")[-1]
     
     final_url = f"{DOMAIN}/{code}"
-
-    resp = client.get(final_url)
+    
+    ref = "https://loan.techyjeeshan.xyz/"
+    
+    h = {"referer": ref}
+  
+    resp = client.get(final_url,headers=h)
+    
     soup = BeautifulSoup(resp.content, "html.parser")
     
-    try: inputs = soup.find(id="go-link").find_all(name="input")
-    except: return "Incorrect Link"
-    
+    inputs = soup.find_all("input")
+   
     data = { input.get('name'): input.get('value') for input in inputs }
 
     h = { "x-requested-with": "XMLHttpRequest" }
     
-    time.sleep(5)
+    time.sleep(4)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
     try:
         return r.json()['url']
