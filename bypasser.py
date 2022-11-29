c93557a228cd85728b7d71b07b935465c4faae00
myexample001 <110930054+myexample001@users.noreply.github.com>
2022-11-29 16:38:39 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -832,31 +832,28 @@ def shareus(url):
 
 def shortlingly(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
-    
-    
-    DOMAIN = "https://shortingly.in/"
+    if 'shortingly.me' in url:
+        DOMAIN = "https://go.techyjeeshan.xyz"
+    else:
+        return "Incorrect Link"
 
     url = url[:-1] if url[-1] == '/' else url
-    
+
     code = url.split("/")[-1]
     
     final_url = f"{DOMAIN}/{code}"
-    
-    ref = "https://go.techyjeeshan.xyz/"
-    
-    h = {"referer": ref}
-  
-    resp = client.get(final_url,headers=h)
-    
+
+    resp = client.get(final_url)
     soup = BeautifulSoup(resp.content, "html.parser")
     
-    inputs = soup.find_all("input")
-   
+    try: inputs = soup.find(id="go-link").find_all(name="input")
+    except: return "A Incorrect Link"
+    
     data = { input.get('name'): input.get('value') for input in inputs }
 
     h = { "x-requested-with": "XMLHttpRequest" }
     
-    time.sleep(1)
+    time.sleep(2)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
     try:
         return r.json()['url']
